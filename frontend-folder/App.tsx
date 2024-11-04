4cd1bd160ef191286d9bb86e92f4370c803d87bb
Luis Henrique Cardoso <luishenri567@gmail.com>
2024-11-04 16:46:07 -0300

    feat: create folders

--- frontend-folder/App.tsx
+++ frontend-folder/App.tsx
@@ -8,17 +8,25 @@ import { AuthContextProvider } from "./src/contexts/AuthContext";
 import 'react-native-reanimated';
 import 'react-native-gesture-handler';
 import themeService from "./src/services/themeService";
+import { MotiView } from "moti";
+
+const splashScreen = require('./src/assets/splashScreen.png');
 
 export default function App() {
   const [theme, setTheme] = useState<typeof darkTheme>(darkTheme);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
+    setIsLoading(true);
+
     themeService.getTheme().then((storedTheme) => {
       if (storedTheme) {
         setTheme(storedTheme === 'dark' ? darkTheme : lightTheme);
       }
     });
-  }, []); 
+
+    setIsLoading(false);
+  }, []);
 
   const toggleTheme = () => {
     setTheme(theme === darkTheme ? lightTheme : darkTheme);
@@ -29,11 +37,11 @@ export default function App() {
   return (
     <AuthContextProvider>
       <StatusBar barStyle={theme === darkTheme ? 'light-content' : 'dark-content'} backgroundColor={theme === darkTheme ? '#0d0921' : '#fefefe'} />
-      <ThemeContext.Provider value={{ toggleTheme }}>
+      < ThemeContext.Provider value={{ toggleTheme }}>
         <ThemeProvider theme={theme}>
           <Routes />
         </ThemeProvider>
-      </ThemeContext.Provider>
-    </AuthContextProvider>
+      </ThemeContext.Provider >
+    </AuthContextProvider >
   )
 }
